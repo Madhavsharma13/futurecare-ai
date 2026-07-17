6075ab2140310746d0c559aeb99cea99b398d7fb
Madhavsharma13 <smaddy880@gmail.com>
2026-07-17 13:37:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// FutureCare Ai JavaScript
+window.onload = function () {
+  console.log("FutureCare AI Website Loaded Successfully!");
+};
+function welcomeMessage() {
+  alert("Welcome to FutureCare Ai! Explore how Artificial Intelligence is transforming healthcare.");
+}
